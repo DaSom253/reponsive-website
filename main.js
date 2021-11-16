1997431de03bbe3ab589a33966c615f9fcae0060
DaSom253 <67749688+DaSom253@users.noreply.github.com>
2021-11-16 16:58:02 +0900

    idontknow

--- main.js
+++ main.js
@@ -54,9 +54,9 @@ document.addEventListener("scroll", () => {
 });
 
 // //arrow-up 투명도 조절
-// document.addEventListener("scroll", () => {
-//     arrowUpBtn.style.opacity = 0 + window.scrollY*homeHeight;
-// });
+arrowUpBtn.addEventListener("scroll", () => {
+    arrowUpBtn.style.opacity = window.scrollY*homeHeight + 0.1;
+ });
 
 //햄버거 메뉴 클릭 시 
 const navbarToggleBtn = document.querySelector(".navbar__toggle-btn");
